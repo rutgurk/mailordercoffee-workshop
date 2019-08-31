a6e4c91b0380fb6d6c40288540d42fbf222c257d
Rutger Korstjens <rutger.korstjens@gmail.com>
2019-08-31 10:15:12 +0200

    Update EspressoWorkshopTest.java
    Find the right views and perform actions by using matchers and methods from the Espresso Core API

--- app/src/androidTest/java/nl/testchamber/mailordercoffeeshop/EspressoWorkshopTest.java
+++ app/src/androidTest/java/nl/testchamber/mailordercoffeeshop/EspressoWorkshopTest.java
@@ -1,5 +1,6 @@
 package nl.testchamber.mailordercoffeeshop;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -16,9 +17,21 @@ public class EspressoWorkshopTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
-    @Test
+    // The @Before annotation let's you run a method before each test
+    // With this annotation the onboarding will be dismissed for each test that we write
+    @Before
     public void dismissOnboarding() {
         onView(withId(R.id.close_button)).perform(click());
-        onView(withId(R.id.custom_order_title)).check(matches(withText("Customize your order")));
+    }
+
+    // For the 'Plus button' I used a hardcoded string. But if a developer has added the text
+    // to the Android resources, then it's also available using R.string.*
+    // but it's still possible to use "Review order" instead of R.string.review_order_button
+    @Test
+    public void orderOverViewShouldDisplayIngredients() {
+        onView(withText("+")).perform(click(), click());
+        onView(withId(R.id.chocolate)).perform(click());
+        onView(withText(R.string.review_order_button)).perform(click());
+        onView(withId(R.id.beverage_detail_ingredients)).check(matches(withText("Ingredients:\n2 shots of espresso\nChocolate")));
     }
 }
