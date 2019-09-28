fab901c5f46dfda994e88dd3c60366cbf373a780
Rutger Korstjens <rutger.korstjens@gmail.com>
2019-09-28 13:33:01 +0200

    add buildvariants and update dependencies
    Added build variants for local and remote data and updated a couple dependencies

---
+++ apiservice/src/localdata/java/nl/testchamber/apiservice/HttpApiService.kt
@@ -0,0 +1,16 @@
+package nl.testchamber.apiservice
+
+import nl.testchamber.apiservice.interfaces.ApiService
+import nl.testchamber.apiservice.interfaces.BrewServiceResponseListener
+import nl.testchamber.apiservice.interfaces.MilkTypeServiceResponseListener
+
+class HttpApiService : ApiService {
+    override fun getBrews(apiServiceResponseListener: BrewServiceResponseListener) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getMilkTypes(milkTypeServiceResponseListener: MilkTypeServiceResponseListener) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
