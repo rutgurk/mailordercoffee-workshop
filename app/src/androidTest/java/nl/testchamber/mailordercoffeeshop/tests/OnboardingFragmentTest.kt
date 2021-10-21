cf1078f6cb665b1fa277d1997b9a23953c1a5c65
Rutger Korstjens <rutger.korstjens@gmail.com>
2021-10-21 13:57:14 +0200

    fixed dependencies to work with targetSDK 31

--- app/src/androidTest/java/nl/testchamber/mailordercoffeeshop/tests/OnboardingFragmentTest.kt
+++ app/src/androidTest/java/nl/testchamber/mailordercoffeeshop/tests/OnboardingFragmentTest.kt
@@ -1,18 +1,27 @@
 package nl.testchamber.mailordercoffeeshop.tests
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.runner.AndroidJUnit4
+import nl.testchamber.mailordercoffeeshop.R
+import nl.testchamber.mailordercoffeeshop.onboarding.OnboardingFragment
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class OnboardingFragmentTest {
 
-//    @Test
-//    fun onboardingTest() {
-//        launchFragmentInContainer {
-//            OnboardingFragment.newInstance("Testname", R.drawable.onboarding_slide_custom_order)
-//        }
-//
-//        onView(withId(R.id.slide_contents)).perform(click())
-//        onView(withId(R.id.slide_contents)).check(matches(withText("Testname")))
-//    }
+    @Test
+    fun onboardingTest() {
+        launchFragmentInContainer {
+            OnboardingFragment.newInstance("Testname", R.drawable.onboarding_slide_custom_order)
+        }
+
+        onView(withId(R.id.slide_contents)).perform(click())
+        onView(withId(R.id.slide_contents)).check(matches(withText("Testname")))
+    }
 }
